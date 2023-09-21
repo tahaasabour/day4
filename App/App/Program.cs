b3a80a16ed457b1121a2f4f8ec9b8a40d6818b74
Taha <Taha@gmail.com>
2023-09-21 10:50:55 +0300

    Till IEnumerator

---
+++ App/App/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections;
+
+public class Program
+{
+    public static void Main()
+    {
+        SuperArray sArr
+             = new SuperArray();
+
+
+        Console.WriteLine(sArr.Count);
+        Console.WriteLine(sArr.Capacity);
+        Console.WriteLine();
+        sArr.Add(1);
+        sArr.Add(10);
+        sArr.Add(500);
+
+        //Console.WriteLine();
+        //Console.WriteLine(sArr.Count);
+        //Console.WriteLine(sArr.Capacity);
+
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        IEnumerator rator = sArr.GetEnumerator();
+        while (rator.MoveNext())
+        {
+            //rator.Current = 10;
+            int i = (int)rator.Current;
+            i = 90;
+            Console.WriteLine(i);
+            Console.WriteLine(i);
+        }
+
+        foreach (int i in sArr)
+        {
+            // i = 90;
+            sArr.Add(6999);
+            Console.WriteLine(i);
+            Console.WriteLine(i);
+        }
+
+
+        //for (int i =0; i< sArr.Count; i++)
+        //    Console.WriteLine(sArr.Get(i));
+
+
+
+
+
+    }
+}
---
+++ App/App/SuperArray.cs
@@ -0,0 +1,69 @@
+﻿
+
+using System.Collections;
+using System;
+
+
+public class SuperArray
+{
+    private int[] list;
+    private int count;
+    public int Capacity
+    { get { return list.Length; } }
+    public int Count
+    {
+        get { return count; }
+    }
+    public SuperArray(int length =1)
+    {
+        count = 0;
+        list = new int[length];
+    }
+    public void Add(int item)
+    {
+        if(count == list.Length)
+        {
+            Array.Resize(ref list, list.Length * 2);
+        }
+        list[count++] = item;
+    }
+    public int Get(int index)
+    {
+        if(index < 0  || index >= list.Length)  
+            
+            throw new IndexOutOfRangeException("index");
+        return list[index];
+    }
+    public IEnumerator GetEnumerator()
+    {
+        return new Enumerator(this);
+    }
+
+    private class Enumerator : IEnumerator
+    {
+        SuperArray list;
+        public Enumerator(SuperArray arr)
+        {
+            list = arr;
+        }
+        public object Current
+        {
+            
+            get 
+            {
+                return list.list[index];
+            }
+        }
+        private int index =-1;
+        public bool MoveNext()
+        {
+            index++;
+            return index < list.count;
+        }
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
