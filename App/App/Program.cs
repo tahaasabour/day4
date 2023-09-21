c7bc89a223bc4f8733a7dbd31f3aa7e694519e21
Taha <Taha@gmail.com>
2023-09-21 12:08:31 +0300

    Collections

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,100 +1,53 @@
 ﻿
 using System;
 using System.Collections;
+using System.Globalization;
 
 public class Program
 {
     public static void Main()
     {
+        ArrayList ls = new ArrayList(90)
+        { 10, "ITI"};
+        ls[0] = 90;
 
-        SuperArray sArr
-             = new SuperArray(10)
-             {
-                 10, 89, 90, "Taha"
-             };
-
-
-
-        sArr[0] = 100;
-
-        Console.WriteLine(sArr[0]);
-        Console.WriteLine(sArr[0]);
-        Console.WriteLine(sArr.Count);
-        Console.WriteLine(sArr.Capacity);
-
-        Console.WriteLine();
-        sArr.Add(1);
-        sArr.Add(10);
-        sArr.Add(500);
-
-        sArr.TrimExcess();
-
-        Console.WriteLine(sArr.Count);
-        Console.WriteLine(sArr.Capacity);
-
-        //Console.WriteLine();
-        //Console.WriteLine(sArr.Count);
-        //Console.WriteLine(sArr.Capacity);
+        Console.WriteLine(ls.Capacity);
+        Console.WriteLine(ls.Count);
+        ls.TrimToSize();
+        Console.WriteLine(ls.Capacity);
+        Console.WriteLine(ls.Count);
+        foreach (object o in ls)
+            Console.WriteLine(o);
 
 
-        Console.WriteLine();
+        Stack s = new Stack();
+        //s[0] -= 90;
+        s.Push(90);
+        s.Push(7);
+        s.Push(79);
+        s.Push(66);
+
+
+        Console.WriteLine(s.Pop());
+        Console.WriteLine(s.Pop());
+
+
+
+        Queue queue = new Queue();
+        queue.Enqueue(10);
+        queue.Enqueue(20);
         Console.WriteLine();
         Console.WriteLine();
 
-
-        //IEnumerator rator = sArr.GetEnumerator();
-        //while (rator.MoveNext())
-        //{
-        //    //rator.Current = 10;
-        //    int i = (int)rator.Current;
-        //    i = 90;
-        //    Console.WriteLine(i);
-        //    Console.WriteLine(i);
-        //}
-
-        foreach (int i in sArr)
-        {
-            Console.WriteLine(i);
-        }
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
 
 
-        //for (int i =0; i< sArr.Count; i++)
-        //    Console.WriteLine(sArr.Get(i));
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine();
-
-        SuperArray Trainees = new SuperArray(6)
-        {
-             new
-            Trainee
-            { Name = "Hany" },
-              new
-            Trainee
-            { Name = "Mostafa" }
-            };
-        foreach(Trainee t in Trainees)
-        {
-            Console.WriteLine(t);
-        }
+        Hashtable ht = new Hashtable();
+        ht.Add(new Trainee() { ID = 1, Name = "Hany" }, "Ahmed");
+        ht.Add(new Trainee() { ID = 1, Name = "Hany" }, "Hany");
 
 
-
-
-        SuperArray sArr4 = new SuperArray();
-        sArr.Add(600);
-        sArr.Add("ITI");
-        sArr.Add(new Trainee() { Name = "Hany"});
-
-
-
-
-        foreach (object obj in sArr4)
-        {
-            if( obj is Trainee)
-                Console.WriteLine(obj);
-        }
 
     }
 
--- App/App/Trainee.cs
+++ App/App/Trainee.cs
@@ -3,8 +3,17 @@
 public class Trainee
 {
     public string Name { get; set; }
+    public int ID { get; set; }
     public override string ToString()
     {
         return Name;
+    }
+    public override bool Equals(object obj)
+    {
+        return ID == ((Trainee)obj).ID;
+    }
+    public override int GetHashCode()
+    {
+        return ID;
     }
 }
